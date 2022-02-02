acd9cb6bfc2e202fbbf9a48c86eb00f6bf159d93
ryland-mueller <ryland.mueller@ucalgary.ca>
2022-02-01 17:50:11 -0700

    Updated dongle prototype
    Added ability to scan and only connect to devices with the TTPS UUIDs

--- ttpms_dongle/src/main.c
+++ ttpms_dongle/src/main.c
@@ -3,10 +3,173 @@
 #include <usb/usb_device.h>
 #include <drivers/uart.h>
 
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <inttypes.h>
+#include <errno.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/scan.h>
+
+#define UUID_PART_1				    0x7f0413db		// randomly generated
+#define TTPMS_LOCATION_FL			0xFF01
+#define TTPMS_LOCATION_FR			0xFF02
+#define TTPMS_LOCATION_RL			0xFF03
+#define TTPMS_LOCATION_RR			0xFF04
+#define BASE_UUID_PART			    0x00FF			// used for base service UUID
+#define UUID_PART_4				    0x9c11			// randomly generated
+#define UUID_PART_5				    0x8f8275ff78c4	// randomly generated
+
+#define TTPMS_BASE_UUID_FL BT_UUID_128_ENCODE(UUID_PART_1, TTPMS_LOCATION_FL, BASE_UUID_PART, UUID_PART_4, UUID_PART_5)
+#define TTPMS_BASE_UUID_FR BT_UUID_128_ENCODE(UUID_PART_1, TTPMS_LOCATION_FR, BASE_UUID_PART, UUID_PART_4, UUID_PART_5)
+#define TTPMS_BASE_UUID_RL BT_UUID_128_ENCODE(UUID_PART_1, TTPMS_LOCATION_RL, BASE_UUID_PART, UUID_PART_4, UUID_PART_5)
+#define TTPMS_BASE_UUID_RR BT_UUID_128_ENCODE(UUID_PART_1, TTPMS_LOCATION_RR, BASE_UUID_PART, UUID_PART_4, UUID_PART_5)
+
+static struct bt_uuid_128 ttpms_uuids[4] = {BT_UUID_INIT_128(TTPMS_BASE_UUID_FL),
+											BT_UUID_INIT_128(TTPMS_BASE_UUID_FR),
+											BT_UUID_INIT_128(TTPMS_BASE_UUID_RL),
+											BT_UUID_INIT_128(TTPMS_BASE_UUID_RR)};
+
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
 	     "Console device is not ACM CDC UART device");
 
-int init_central(void);
+static struct bt_conn *default_conn;
+
+static void scan_filter_match(struct bt_scan_device_info *device_info,
+			      struct bt_scan_filter_match *filter_match,
+			      bool connectable)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(device_info->recv_info->addr, addr, sizeof(addr));
+
+	printk("Filters matched. Address: %s connectable: %s\n",
+		addr, connectable ? "yes" : "no");
+}
+
+static void scan_connecting_error(struct bt_scan_device_info *device_info)
+{
+	printk("Connecting failed\n");
+}
+
+static void scan_connecting(struct bt_scan_device_info *device_info,
+			    struct bt_conn *conn)
+{
+	default_conn = bt_conn_ref(conn);
+}
+
+static void scan_filter_no_match(struct bt_scan_device_info *device_info,
+				 bool connectable)
+{
+	int err;
+	struct bt_conn *conn;
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	if (device_info->recv_info->adv_type == BT_GAP_ADV_TYPE_ADV_DIRECT_IND) {
+		bt_addr_le_to_str(device_info->recv_info->addr, addr,
+				  sizeof(addr));
+		printk("Direct advertising received from %s\n", addr);
+		bt_scan_stop();
+
+		err = bt_conn_le_create(device_info->recv_info->addr,
+					BT_CONN_LE_CREATE_CONN,
+					device_info->conn_param, &conn);
+
+		if (!err) {
+			default_conn = bt_conn_ref(conn);
+			bt_conn_unref(conn);
+		}
+	}
+}
+
+BT_SCAN_CB_INIT(scan_cb, scan_filter_match, scan_filter_no_match,
+		scan_connecting_error, scan_connecting);
+
+static void connected(struct bt_conn *conn, uint8_t conn_err)
+{
+	int err;
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (conn_err) {
+		printk("Failed to connect to %s (%u)\n", addr, conn_err);
+		if (conn == default_conn) {
+			bt_conn_unref(default_conn);
+			default_conn = NULL;
+
+			/* This demo doesn't require active scan */
+			err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+			if (err) {
+				printk("Scanning failed to start (err %d)\n",
+				       err);
+			}
+		}
+
+		return;
+	}
+
+	printk("Connected: %s\n", addr);
+
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+	int err;
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Disconnected: %s (reason %u)\n", addr, reason);
+
+	if (default_conn != conn) {
+		return;
+	}
+
+	bt_conn_unref(default_conn);
+	default_conn = NULL;
+
+	/* This demo doesn't require active scan */
+	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+	if (err) {
+		printk("Scanning failed to start (err %d)\n", err);
+	}
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static void scan_init(void)
+{
+	int err;
+
+	struct bt_scan_init_param scan_init = {
+		.connect_if_match = 1,
+		.scan_param = NULL,
+		.conn_param = BT_LE_CONN_PARAM_DEFAULT
+	};
+
+	bt_scan_init(&scan_init);
+	bt_scan_cb_register(&scan_cb);
+
+	err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_UUID, ttpms_uuids);
+	if (err) {
+		printk("Scanning filters cannot be set (err %d)\n", err);
+
+		return;
+	}
+
+	err = bt_scan_filter_enable(BT_SCAN_UUID_FILTER, false);
+	if (err) {
+		printk("Filters cannot be turned on (err %d)\n", err);
+	}
+}
 
 void main(void)
 {
@@ -14,15 +177,35 @@ void main(void)
 	uint32_t dtr = 0;
 
 	if (usb_enable(NULL)) {
-		return;		// kill the program if USB enable fails
+		return;
 	}
 
-	/* Wait for the DTR flag to be set (meaning serial is ready) */
+	/* Poll if the DTR flag was set */
 	while (!dtr) {
 		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
 		/* Give CPU resources to low priority threads. */
-		k_sleep(K_MSEC(50));
+		k_sleep(K_MSEC(100));
 	}
 
-	(void)init_central();
+	int err;
+
+	printk("Starting TTPMS Dongle\n");
+
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	scan_init();
+
+	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+	if (err) {
+		printk("Scanning failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Scanning successfully started\n");
 }
